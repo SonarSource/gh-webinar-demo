5fbb69f67b217fe0d312200fe86e44e47bfcf9fe
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2019-09-10 12:09:09 +0200

    Support analysis of PR

---
+++ runAnalysis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo '======= Analyze only master'
+  mvn clean verify sonar:sonar -Pcoverage \
+    -Dsonar.host.url=https://next.sonarqube.com/sonarqube/
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo '======= Analyze pull request'
+  mvn clean verify sonar:sonar -Pcoverage \
+    -Dsonar.host.url=https://next.sonarqube.com/sonarqube/ \
+    -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH \
+    -Dsonar.pullrequest.base=$TRAVIS_BRANCH \
+    -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST \
+    -Dsonar.pullrequest.provider=github \
+    -Dsonar.pullrequest.github.repository=$TRAVIS_REPO_SLUG \
+fi
+
+
+
