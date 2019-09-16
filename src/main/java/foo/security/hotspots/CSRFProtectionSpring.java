49157ebe375f1837d112ed2c50af50815649476b
agigleux <alexandre.gigleux@sonarsource.com>
2019-09-16 09:12:58 +0200

    Add Vuln+Hotspots on New Code

---
+++ src/main/java/foo/security/hotspots/CSRFProtectionSpring.java
@@ -0,0 +1,14 @@
+package foo.security.hotspots;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@EnableWebSecurity
+public class CSRFProtectionSpring extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+}
--- src/main/java/foo/security/injection/Servlet.java
+++ src/main/java/foo/security/injection/Servlet.java
@@ -16,9 +16,15 @@ public class Servlet extends HttpServlet {
   private static final String FIELD_NAME = "FIELD_KEY";
 
   @Override
-  public void doPost(HttpServletRequest taintedRequest, HttpServletResponse response)
-      throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setHeader("Content-Type", "text/plain; charset=utf-8");
+    resp.setHeader("Access-Control-Allow-Origin", "*"); // Questionable
+    resp.getWriter().write("Sensitive Information");
+  }
+
+  @Override
+  public void doPost(HttpServletRequest taintedRequest, HttpServletResponse response) throws ServletException, IOException {
+    response.setHeader("Content-Type", "text/html; charset=UTF-8");   
 
     String taintedString = "";
     if (taintedRequest.getHeader("HeaderName") != null) {
@@ -29,13 +35,12 @@ public class Servlet extends HttpServlet {
     try {
       taintedString = java.net.URLDecoder.decode(taintedString, "UTF-8");
 
-      new SQLInjectionVulnerability(taintedString);
       new CommandInjectionVulnerability(taintedString);
       new RegexInjectionVulnerability(taintedString);
 
       String[] array = BusinessThingsUtils.doComplexBusinessThings(taintedString);
-      new SQLInjectionVulnerabilityArray3Files(array[0]); // Safe
-      new SQLInjectionVulnerabilityArray3Files(array[1]); // Noncompliant: array[1] has been tainted in doComplexBusinessThings
+      new SQLInjectionVulnerabilityCollectionMultipleFiles(array[0]); // Safe
+      new SQLInjectionVulnerabilityCollectionMultipleFiles(array[1]); // Noncompliant: array[1] has been tainted in doComplexBusinessThings
 
     } catch (SQLException|IOException e) {
     }
