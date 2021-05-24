a360df5f1ba40f8a20c9dd9be2a23d6defd2060d
Duarte Meneses <duarte.meneses@sonarsource.com>
2021-05-24 10:44:21 -0500

    Add debug travis build

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -2,11 +2,11 @@
 
 if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo '======= Analyze only master'
-  mvn clean verify sonar:sonar -Pcoverage \
+  mvn clean verify sonar:sonar -e -X -Pcoverage \
     -Dsonar.host.url=https://next.sonarqube.com/sonarqube/
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo '======= Analyze pull request'
-  mvn clean verify sonar:sonar -Pcoverage \
+  mvn clean verify sonar:sonar -X -e -Pcoverage \
     -Dsonar.host.url=https://next.sonarqube.com/sonarqube/ \
     -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH \
     -Dsonar.pullrequest.base=$TRAVIS_BRANCH \
